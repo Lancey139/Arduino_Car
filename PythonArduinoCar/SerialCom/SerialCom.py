294548569cf1519d4dc97bc41e5ad9fe002b8d90
Nicolas MEO <nicolas.meo@gmail.com>
2018-10-07 22:27:04 +0200

    Ajout de la liason série avec l'arduino

---
+++ PythonArduinoCar/SerialCom/SerialCom.py
@@ -0,0 +1,17 @@
+'''
+Created on 7 oct. 2018
+
+@author: Nicolas
+'''
+
+class SerialCom(object):
+    '''
+    Classe permettant la communication série avec l'arduino
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        pass
